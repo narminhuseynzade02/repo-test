6936592c62f84efe0c12109b1a1d8c3c3c888e04
Kerim <alekperovkerim@gmail.com>
2023-04-13 17:45:00 +0400

    hometask

---
+++ main.js
@@ -0,0 +1,48 @@
+//let a = 10
+//for (a; a >= 5 ; a -- ) {
+//    console.log(a);
+//}
+// task1
+// function registrProcess() { 
+//     const lastName = prompt("Please write your last name:"); 
+//     const firstName = prompt("Please write your first name:"); 
+//     const email = prompt("Please write your email address:"); 
+//     return `${email} - ${lastName} ${firstName}`; 
+//   } 
+// console.log(registrProcess());
+// task2
+// function calculator(){
+//     const distance = +prompt("Please write distance in km : ");
+//     const fixedcost = 5
+//     return(distance*0.25) + fixedcost;
+// }
+// console.log(calculator());
+// task3
+// function calculateReceiptAmount(name , UnitPrice , quantity , ParticipatesInPromotion) {
+//     let amount = UnitPrice * quantity; 
+//     if (quantity >= 5) {
+//         amount *= 0.9; 
+//       }
+//       if (ParticipatesInPromotion) {
+//         amount *= 0.85;
+//       }
+//       return amount;
+// }
+// console.log(calculateReceiptAmount("item1", 8, 16, true))
+// task4
+// function suggestTable(numGuests) {
+//     if (numGuests<=2) {
+//         return "You can be seated a small table.";   
+//     } else if (numGuests<=4){
+//         return "You can be seated a medium table.";   
+//     }else if (numGuests<=8){
+//         return "You can be seated a large table.";   
+//     }else {
+//         return "You can not be seated a suitable table.";   
+//     }
+//     }
+    
+// console.log(suggestTable(9)) 
+function printDescendingOrder(num1 , num2 , num3){
+    
+}
